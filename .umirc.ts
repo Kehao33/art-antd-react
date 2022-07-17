9474185f505cb10f59479adafc84505af13cfb0b
JakeQuc <3121894148@qq.com>
2022-07-17 10:28:41 +0800

    update config

--- .umirc.ts
+++ .umirc.ts
@@ -4,7 +4,12 @@ export default defineConfig({
   title: 'art-antd-react',
   favicon: '/icons/logo.ico',
   logo: '/icons/logo.png',
-  outputPath: 'docs-dist',
+
+  outputPath: '/docs-dist',
+  base: '/docs-dist',
+  publicPath: './docs-dist/',
+
+  exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   mode: 'site',
   // more config: https://d.umijs.org/config
   antd: {},
